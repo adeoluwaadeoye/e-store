a09270504ce3503ff5da9be78946906ce3ac48b2
Adeoluwa Adeoye <adeoluadeoye7@gmail.com>
2024-11-24 20:30:15 +0100

    fixed description details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -55,9 +55,9 @@ const ProductDetails = () => {
               $ {price}
             </div>
             <p className='mb-8'>
-              {truncateDescription(description, 30)}
+              {truncateDescription(description, 25)}
             </p>
-            <button onClick={() => addToCart(product, product.id)} className='bg-primary py-4 px-8 text-white'>
+            <button onClick={() => addToCart(product, product.id)} className='bg-primary py-4 px-8 mb-4 text-white'>
               Add to cart
             </button>
           </div>
